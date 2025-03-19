3dd7a4b2dded8fbe7997f8c3e81e56fc124bdcb3
Paul Paterson <ptpaterson@gmail.com>
2025-03-19 14:56:00 -0400

    tweak home page for big screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,47 +7,48 @@ export default async function Home() {
   const posts = await listBlogPosts()
 
   return (
-    <div className='from-primary/10 via-surface-2 to-primary/10 mx-auto h-full bg-linear-160 px-6 py-12'>
-      {/* Hero Section */}
-      <section className='flex flex-col items-center gap-8 md:flex-row'>
-        <div className='md:w-1/2'>
-          <h1 className='mb-4 flex gap-2 text-4xl font-bold md:block md:text-5xl'>
-            <div>Hi, I&apos;m</div>{' '}
-            <div className='text-primary'>Paul Paterson</div>
-          </h1>
-          <p className='mb-6 text-xl'>
-            I am passionate about building things, whether it&apos;s with code
-            or with my hands. Sometimes I write about it.
-          </p>
-        </div>
-        <div className='p-4 md:w-1/2'>
-          <Image
-            src='/MeFace.jpg?height=250&width=250'
-            alt='Profile'
-            width={250}
-            height={250}
-            className='rounded-full'
-          />
-        </div>
-      </section>
+    <div className='from-primary/10 via-surface-2 to-primary/10 h-full w-full bg-linear-160 px-6 py-12'>
+      <div className='mx-auto max-w-[1024px] md:px-12'>
+        {/* Hero Section */}
+        <section className='flex flex-col items-center gap-8 md:flex-row'>
+          <div className='md:w-1/2'>
+            <h1 className='mb-4 flex gap-2 text-4xl font-bold md:block md:text-5xl'>
+              <div>Hi, I&apos;m</div>{' '}
+              <div className='text-primary'>Paul Paterson</div>
+            </h1>
+            <p className='mb-6 text-xl'>
+              I am passionate about building things, whether it&apos;s with code
+              or with my hands. Sometimes I write about it.
+            </p>
+          </div>
+          <div className='p-4'>
+            <Image
+              src='/MeFace.jpg?height=250&width=250'
+              alt='Profile'
+              width={250}
+              height={250}
+              className='rounded-full'
+            />
+          </div>
+        </section>
 
-      {/* Recent Posts */}
-      <section className='mt-12'>
-        <div className='mb-8'>
-          <h2 className='mb-4 text-3xl font-bold'>Recent Posts</h2>
-          <p className=''>
-            I am still working on more content for the blog, but for now, check
-            out my available posts!
-          </p>
-        </div>
-        <div className='grid-col-1 grid gap-6 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4'>
-          {posts.map((post) => (
-            <article
-              key={post.slug}
-              className='bg-surface-2 border-border relative isolate rounded-lg border shadow-lg'
-            >
-              <div className='hover:bg-primary/10 flex h-full flex-col overflow-hidden rounded-lg transition'>
-                {/* <Image
+        {/* Recent Posts */}
+        <section className='mt-12'>
+          <div className='mb-8'>
+            <h2 className='mb-4 text-3xl font-bold'>Recent Posts</h2>
+            <p className=''>
+              I am still working on more content for the blog, but for now,
+              check out my available posts!
+            </p>
+          </div>
+          <div className='grid-col-1 grid gap-6 sm:grid-cols-2 lg:grid-cols-3'>
+            {posts.map((post) => (
+              <article
+                key={post.slug}
+                className='bg-surface-2 border-border relative isolate rounded-lg border shadow-lg'
+              >
+                <div className='hover:bg-primary/10 flex h-full flex-col overflow-hidden rounded-lg transition'>
+                  {/* <Image
                   src={`/banners/${
                     post.metadata.banner ?? 'file.svg'
                   }?height=200&width=400`}
@@ -56,27 +57,28 @@ export default async function Home() {
                   height={200}
                   className='bg-surface-2 h-48 w-full rounded-t-lg object-cover'
                 /> */}
-                <div className='flex grow flex-col p-4'>
-                  <h3 className='mb-2 text-xl font-semibold'>
-                    <Link href={`/blog/${post.slug}`}>
-                      <span className='absolute inset-0 z-10'></span>
-                      {post.metadata.title}
-                    </Link>
-                  </h3>
-                  <Time
-                    className='text-muted-foreground order-first'
-                    date={post.metadata.updatedDate ?? post.metadata.date}
-                  />
-                  <p className='mb-2'>{post.metadata.description}</p>
-                  <div aria-hidden='true' className='text-primary mt-auto'>
-                    Read article →
+                  <div className='flex grow flex-col p-4'>
+                    <h3 className='mb-2 text-xl font-semibold'>
+                      <Link href={`/blog/${post.slug}`}>
+                        <span className='absolute inset-0 z-10'></span>
+                        {post.metadata.title}
+                      </Link>
+                    </h3>
+                    <Time
+                      className='text-muted-foreground order-first'
+                      date={post.metadata.updatedDate ?? post.metadata.date}
+                    />
+                    <p className='mb-2'>{post.metadata.description}</p>
+                    <div aria-hidden='true' className='text-primary mt-auto'>
+                      Read article →
+                    </div>
                   </div>
                 </div>
-              </div>
-            </article>
-          ))}
-        </div>
-      </section>
+              </article>
+            ))}
+          </div>
+        </section>
+      </div>
     </div>
   )
 }
